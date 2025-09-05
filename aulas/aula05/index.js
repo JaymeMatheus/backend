7d0d6bfd8ef5680ed7793eb5544c4959e43cdb0a
Jayme Matheus <jaymematheus.2020@gmail.com>
2025-09-04 21:45:18 -0300

    adicionando os codigos da aula05

---
+++ aulas/aula05/index.js
@@ -0,0 +1,45 @@
+// importa o framework
+const express = require("express");
+
+// cria uma instancia da aplicação
+const app = express();
+
+// middleware de aplicação
+app.use((req, res, next) => {
+    console.log("Passei aqui");
+    next();
+})
+
+// middleware de rota
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("Chegou aqui");
+});
+
+router.post('/', (req, res) => {
+    res.status(201).send("Inserido com sucesso");
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params; // {id: 1, param2: 5, param3: 6}
+    if (id == 1) return res.send("Achei");
+    throw Error("Não achei");
+})
+
+app.use('/tarefas', router);
+
+// middleware de erro
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).send("Algo de errado não está certo!");
+});
+
+// app((req, res) => {
+//    
+//}) 
+
+// inicia a aplicação
+app.listen(3000, ()=>{
+    console.log("App está ON!");
+});
